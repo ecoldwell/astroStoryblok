f87c27ccb3c0f7011a042f8fd1ea5e8264802ebe
ecoldwell <emmalcoldwell@gmail.com>
2024-06-12 15:56:56 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from "@astrojs/tailwind"
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+ 
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "ca",
+      },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind()
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
